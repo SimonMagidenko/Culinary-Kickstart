34f7ecbdadc4cc05c89475e10cb8cd676b94ae0c
SimonM <smagidenko@gmail.com>
2023-10-05 14:19:27 -0400

    Implemented navbar on all pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -49,11 +49,11 @@ function App() {
   return (
     <ChakraProvider theme={theme}>
       <ApolloProvider client={client}>
-        <div className="flex-column justify-flex-start min-100-vh">
-          <div className="container">
-            <Outlet />
-          </div>
-        </div>
+        {/* <div className="flex-column justify-flex-start min-100-vh">
+          <div className="container"> */}
+        <Outlet />
+        {/* </div>
+        </div> */}
       </ApolloProvider>
     </ChakraProvider>
   );
