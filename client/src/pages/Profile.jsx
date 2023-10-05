513e1f29950e37ed9a7c2b0bd081e82d01bfdea8
dylanstoudt <138310076+dylanstoudt@users.noreply.github.com>
2023-10-05 14:00:07 -0400

    final changes

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -32,9 +32,7 @@ const Profile = () => {
 
           <Center py={6}>
             <Box maxW={'450'}w={'90vw'} maxH={'1000px'} h={'full'} bg={useColorModeValue('white', 'gray.800')} boxShadow={'2xl'} rounded={'md'} overflow={'hidden'}>
-              <Image
-                h={'450px'}
-                w={'full'}
+              <Image h={'450px'} w={'full'}
                 src={
                   'https://images.unsplash.com/photo-1612865547334-09cb8cb455da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
                 }
