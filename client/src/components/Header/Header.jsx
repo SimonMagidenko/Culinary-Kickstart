9691e1e50e82ce333ab155c6344f8cc1d34a2837
SimonM <smagidenko@gmail.com>
2023-10-06 14:28:09 -0400

    finished formattign and merging

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -16,57 +16,24 @@ import {
   useColorModeValue,
   Stack,
 } from "@chakra-ui/react";
-
+import "./Header.css";
 import { HamburgerIcon, CloseIcon, AddIcon } from "@chakra-ui/icons";
 import { Link } from "react-router-dom";
-
-const Links = ["Culinary Kickstart"];
-
-const NavLink = (props) => {
-  const { children } = props;
-  return (
-    <Box
-      as="a"
-      px={2}
-      py={1}
-      rounded={"md"}
-      _hover={{
-        textDecoration: "none",
-        bg: useColorModeValue("gray.200", "gray.700"),
-      }}
-      href={"#"}
-    >
-      {children}
-    </Box>
-  );
-};
+import * as LuIcon from "react-icons/lu";
 
 export default function HeaderBar() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
   return (
     <>
-      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+      <Box bg={useColorModeValue("#333333")} px={4}>
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-          <IconButton
-            size={"md"}
-            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={"Open Menu"}
-            display={{ md: "none" }}
-            onClick={isOpen ? onClose : onOpen}
-          />
-          <HStack spacing={8} alignItems={"center"}>
-            <Box>Logo</Box>
-            <HStack
-              as={"nav"}
-              spacing={4}
-              display={{ base: "none", md: "flex" }}
-            >
-              {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
-              ))}
-            </HStack>
-          </HStack>
+          <div className="logoTitle">
+            <Link to={"/"}>
+              <LuIcon.LuChefHat size={"35px"} />
+            </Link>
+            <Link to={"/"}>
+              <h1>Culinary Kickstart</h1>
+            </Link>
+          </div>
           <Flex alignItems={"center"}>
             <Link to={"/login"}>
               <Button variant={"solid"} colorScheme={"teal"} size={"sm"} mr={4}>
@@ -98,16 +65,6 @@ export default function HeaderBar() {
             </Menu>
           </Flex>
         </Flex>
-
-        {isOpen ? (
-          <Box pb={4} display={{ md: "none" }}>
-            <Stack as={"nav"} spacing={4}>
-              {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
-              ))}
-            </Stack>
-          </Box>
-        ) : null}
       </Box>
     </>
   );
--- client/src/components/RecipeCard/RecipeCard.jsx
+++ client/src/components/RecipeCard/RecipeCard.jsx
@@ -1,57 +1,59 @@
 import {
-    Button,
-    Checkbox,
-    Flex,
-    Text,
-    FormControl,
-    FormLabel,
-    Heading,
-    Input,
-    HStack,
-    Image,
-    Card,
-    CardBody,
-    CardFooter,
-    Stack
+  Button,
+  Checkbox,
+  Flex,
+  Text,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  HStack,
+  Image,
+  Card,
+  CardBody,
+  CardFooter,
+  Stack,
 } from "@chakra-ui/react";
 
 const RecipeCard = (props) => {
-    return (
-        <>
-            {props.recipes.map((data) => {
+  return (
+    <>
+      {props.recipes.map((data, i) => {
+        return (
+          <Card
+            key={i}
+            direction={{ base: "column", sm: "row" }}
+            overflow="hidden"
+            variant="outline"
+          >
+            <Image
+              objectFit="cover"
+              maxW={{ base: "100%", sm: "200px" }}
+              src={data.recipe.image}
+              alt="Caffe Latte"
+            />
 
-                return <Card
-                    direction={{ base: 'column', sm: 'row' }}
-                    overflow='hidden'
-                    variant='outline'
-                >
-                    <Image
-                        objectFit='cover'
-                        maxW={{ base: '100%', sm: '200px' }}
-                        src={data.recipe.image}
-                        alt='Caffe Latte'
-                    />
+            <Stack>
+              <CardBody>
+                <Heading size="md">{data.recipe.label}</Heading>
 
-                    <Stack>
-                        <CardBody>
-                            <Heading size='md'>{data.recipe.label}</Heading>
+                <Text py="2">
+                  Caffè latte is a coffee beverage of Italian origin made with
+                  espresso and steamed milk.
+                </Text>
+              </CardBody>
 
-                            <Text py='2'>
-                                Caffè latte is a coffee beverage of Italian origin made with espresso
-                                and steamed milk.
-                            </Text>
-                        </CardBody>
-
-                        <CardFooter>
-                            <Button variant='solid' colorScheme='blue'>
-                                Save Recipe
-                            </Button>
-                        </CardFooter>
-                    </Stack>
-                </Card>
-            })}
-        </>
-    )
-}
+              <CardFooter>
+                <Button variant="solid" colorScheme="blue">
+                  Save Recipe
+                </Button>
+              </CardFooter>
+            </Stack>
+          </Card>
+        );
+      })}
+    </>
+  );
+};
 
 export default RecipeCard;
