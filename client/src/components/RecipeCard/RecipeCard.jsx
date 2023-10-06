a6f1544e77de743e86d7722392d239eebf43be42
HarryFelty <harryfelty@gmail.com>
2023-10-06 13:45:57 -0400

    pushing to merge

--- client/src/components/RecipeCard/RecipeCard.jsx
+++ client/src/components/RecipeCard/RecipeCard.jsx
@@ -28,7 +28,7 @@ const RecipeCard = (props) => {
                     <Image
                         objectFit='cover'
                         maxW={{ base: '100%', sm: '200px' }}
-                        src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
+                        src={data.recipe.image}
                         alt='Caffe Latte'
                     />
 
